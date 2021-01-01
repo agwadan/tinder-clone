61f285724bfc84e4d38ae7fea7202aedddf84b6a
agwadan <agwa.dan@gmail.com>
2021-01-01 18:09:40 +0300

    Setup header with icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <h3>Tinder Clone</h3>
+
+       
+      {/* Header */}
+      <Header />
+
+
     </div>
   );
 }
