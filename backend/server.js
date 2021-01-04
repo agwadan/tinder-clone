d0acc07a3f3b4d206d108d0b5dfb4f26e7c39312
agwadan <agwa.dan@gmail.com>
2021-01-04 14:42:31 +0300

    Established connection with db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
-const mongoose  = require("mongoose");
 const express   = require("express");
+const mongoose  = require("mongoose");
+require ('dotenv').config();
 
 //App config
 const app   = express();
-const port  = process.env.PORT || 5001 ;
+const port  = process.env.PORT || 5001;
 
 //middleware
 
 
 //db config
+
+const uri   = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+                    useNewUrlParser: true,
+                    useCreateIndex: true,
+                    useUnifiedTopology: true
+                  }
+                )
+const connection = mongoose.connection;
+console.log("Mongoose... Connection successfully established.");
 
 //api endpoints
 app.get('/', (req, res) => {
